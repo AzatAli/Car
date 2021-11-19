9bf808f14ad8d7034ce74376db5f8847a71011db
AzatAli <52886387+AzatAli@users.noreply.github.com>
2021-11-19 22:28:21 +0700

    Create Car main.py

---
+++ Car main.py
@@ -0,0 +1,46 @@
+class Car:
+        def __init__(self, model, size, color, doors, mileage):
+                self.model = model
+                self.size = size
+                self.color = color
+                self.doors = doors
+                self.mileage = mileage
+        def characteristics(self):
+                print("\nХарактеристики вашего автомобиля:\nМарка: ", self.model, "\nРазмер: ", self.size, "\nЦвет: ", self.color, "\nКол-во дверей: ", self.doors, "\nПробег: ", self. mileage, "\n")
+
+models = [" ","Nissan", "Toyota", "BMW", "Lamborgini", "Ford", "Ferrari"]
+sizes = [" ", "Big", "Medium", "Small", "Family size"]
+colors = [" ", "Red", "Yellow", "Blue", ]
+doorss = [" ", "2", "4", "6"]
+mileages = [" ", "НОВАЯ", "До 1000 км", ">1000 км"]
+
+car_user_choice = Car
+print("Здравствуйте, позвольте нашему сервису подобрать вам машину.\nМы дадим вам выбор из характеристик, подобрав которые вы сможете получить готовую машину\n")
+
+def chce(x, options, liste):
+        print("\nВыберите ", x)
+        global choice
+        choice = int(input(options))
+
+chce("модель вашей машины", "\n1. Nissan\n2. Toyota\n3. BMW\n4. Lamborgini\n5. Ford\n6. Ferrari\n", models)
+car_user_choice.model = choice
+print("Ваш выбор: ", car_user_choice.model)
+
+chce("размер вашей машины", "1. Big\n2. Medium\n3. Small\n4. Family size\n", sizes)
+car_user_choice.size = choice
+print("Ваш выбор: ", car_user_choice.size)
+
+chce("кол-во дверей:", "\n1. 2\n2. 4\n3. 6\n", doorss)
+car_user_choice.doors = choice
+print("Ваш выбор: ", car_user_choice.doors)
+
+chce("цвет:", "\n1. Red\n2. Yellow\n3. Blue\n", colors)
+car_user_choice.color = choice
+print("Ваш выбор: ", car_user_choice.color)
+
+chce("пробег", "\n1. НОВАЯ\n2. До 1000км\n3. >1000км\n", mileages)
+car_user_choice.mileage = choice
+print("Ваш выбор: ", car_user_choice.mileage)
+
+car_user_choice.characteristics(car_user_choice)
+input()
